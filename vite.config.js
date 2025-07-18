88cf4cbc7eb90c41b09e2caa351ff71b00e63459
pankaj1132 <pankaj17469@gmail.com>
2025-07-18 11:11:36 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react()],
+  base: '/', // For Vercel asset loading
+  build: {
+    outDir: 'dist', // Vercel expects this
+  },
+});
